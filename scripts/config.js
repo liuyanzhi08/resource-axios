09e8a88894f32a30ca7a4c6004b1991591d229d0
liuyanzhi08 <702368372@qq.com>
2018-04-12 01:22:18 +0800

    update rollup config

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,8 @@
 // rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+
 export default {
   input: 'src/resource-axios.js',
   output: {
@@ -6,6 +10,8 @@ export default {
     format: 'cjs',
   },
   plugins: [
-
+    resolve(),
+    commonjs(),
+    json(),
   ],
 };
--- src/resource-axios.js
+++ src/resource-axios.js
@@ -14,6 +14,7 @@
  */
 
 import axios from 'axios';
+// console.log(axios)
 
 export default (path, actions) => {
   const resource = {
@@ -22,7 +23,7 @@ export default (path, actions) => {
     save: data => axios.post(path, data),
     update: (id, data) => axios.put(`${path}/${id}`, data),
     delete: id => axios.delete(`${path}/${id}`),
-  }
+  };
   return Object.assign(resource, actions);
 };
 
