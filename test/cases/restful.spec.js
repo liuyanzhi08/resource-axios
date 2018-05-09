58bc00bf0e2c4cf80861768239dab7fe40570c91
liuyanzhi <liuyanzhi@gf.com.cn>
2018-05-09 10:49:39 +0800

    remove sinon

--- test/cases/restful.spec.js
+++ test/cases/restful.spec.js
@@ -1,23 +1,18 @@
 import axios from 'axios';
 import chai from 'chai';
 import resource from '../../dist/resource-axios';
-import makeSuite from '../helpers';
 
 const { expect } = chai;
 
-// describe('Restful methods', () => {
-//   let Baidu;
-//   before(() => {
-//     Baidu = resource('http://baidu.com', axios);
-//   });
-//
-//   it('should work', async () => {
-//     await Baidu.get().then((res) => {
-//       expect(res.data).to.be.a('string');
-//     });
-//   }).timeout(30000);
-// });
+describe('Restful methods', () => {
+  let Baidu;
+  before(() => {
+    Baidu = resource('http://baidu.com', axios);
+  });
 
-makeSuite('Restful methods', () => {
-  expect(1).to.be.a('number');
-})
+  it('should work', async () => {
+    await Baidu.get().then((res) => {
+      expect(res.data).to.be.a('string');
+    });
+  }).timeout(30000);
+});
